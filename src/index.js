bccebaef238311bcf69af1ae4cf49fa288f70dda
donkeyhuang <hgpatswu@gmail.com>
2017-09-14 11:41:43 +0800

    initial import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Timer from './Timer'
+import Timer from './users/Timer'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<div> <App /> <Timer /> </div>, document.getElementById('root'));
